94890e9047f1b3cd4975d1a92a3b292d96cbcf6a
UnLuckiestClovr <urluckyclovr@gmail.com>
2024-09-30 11:28:03 -0600

    Created Database scripts for Basket Service

--- CapstoneVariant/Backend/BasketService/DAL/database-interaction.py
+++ CapstoneVariant/Backend/BasketService/DAL/database-interaction.py
@@ -77,3 +77,73 @@ def getBasket(BID: str):
         }
 
 
+def deleteItemFromBasket(BID: str, IID: str):
+    try:
+        rConn = redis.Redis(connection_pool=rConnPool)
+
+        # Get all elements from the list
+        items = rConn.lrange(f'{BID}:Items', 0, rConn.llen(f'{BID}:Items'))
+
+        if items:
+            # Decode to Python Dicts
+            itemsDecoded = [item.decode('utf-8') for item in items]
+            itemObjects = [json.loads(item) for item in itemsDecoded]
+            print(itemObjects)
+
+            #iterate through list to find then remove the item by checking item IID
+            updatedItemObjects = [item for item in itemObjects if item.get('IID') != IID]
+
+            print(updatedItemObjects)
+
+            if len(itemObjects) != len(updatedItemObjects):
+                #encode back into strings for redis to use
+                updatedItems = [json.dumps(item) for item in updatedItemObjects]
+
+                #update redis data
+                rConn.delete(f'{BID}:Items')
+                if updatedItems:
+                    rConn.rpush(f'{BID}:Items', *updatedItems)
+
+                    return {
+                        "success": True,
+                        "message": f"Basket \'{BID}\' updated successfully"
+                    }
+            return {
+                "success": False,
+                "message": f"Could not find Item of IID : {IID}"
+            }
+        else:
+            return {
+                "success" : False,
+                "message" : f"Could not find Basket of UID : {BID}"
+            }
+    except redis.ConnectionError as e:
+        return {
+            "success": False,
+            "message": f"Error connecting to Redis: {e}"
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "message": f"An error occurred: {e}"
+        }
+
+
+def ClearBasket(BID: str):
+    try:
+        rConn = redis.Redis(connection_pool=rConnPool)
+        rConn.delete(f'{BID}:Items')
+        return {
+            "success" : True,
+            "message" : f"Successful Deletion of Basket : {BID}"
+        }
+    except redis.ConnectionError as e:
+        return {
+            "success": False,
+            "message": f"Error connecting to Redis: {e}"
+        }
+    except Exception as e:
+        return {
+            "success": False,
+            "message": f"An error occurred: {e}"
+        }
