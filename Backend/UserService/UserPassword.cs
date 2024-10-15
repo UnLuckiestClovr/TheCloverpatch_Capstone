071f5f4570f38bce40aa36a5ecf8cf0beb9e0d01
UnLuckiestClovr <urluckyclovr@gmail.com>
2024-10-15 12:00:42 -0600

    Register Works with SaltHashing Passwords for Storage; Database and Tables are created automatically if they do not exist at the time of accessing endpoints.

--- Backend/UserService/UserPassword.cs
+++ Backend/UserService/UserPassword.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BCrypt.Net;
 
 public class UserPassword
 {
@@ -9,7 +10,19 @@ public class UserPassword
 
     public UserPassword(string id, string password) { 
         this.ID = id;
-        this.Password = password;
+        this.Password = HashPassword(password);
+    }
+
+// Hashes Password for more secure Storage and Usage
+    private string HashPassword(string password) 
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+
+// Compares non Hashed Password to currently stored Hash Password
+    public bool VerifyPassword(string password)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, this.Password);
     }
 
 }
