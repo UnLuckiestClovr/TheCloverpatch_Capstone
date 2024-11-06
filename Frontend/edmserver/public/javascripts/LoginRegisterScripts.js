20abff72e5f8496c6ba672e56e5ccc1acefbe027
UnLuckiestClovr <urluckyclovr@gmail.com>
2024-11-06 12:20:43 -0700

    Continuing Small Changes: Moving String Validation Checks

--- Frontend/edmserver/public/javascripts/LoginRegisterScripts.js
+++ Frontend/edmserver/public/javascripts/LoginRegisterScripts.js
@@ -59,46 +59,6 @@ try {
     })
 
 
-    function validateUsername(username) {
-        const usernameRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
-        return usernameRegex.test(username);
-    }
-    
-    function validatePassword(password) {
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-]).+$/;
-        return passwordRegex.test(password);
-    }
-
-    function validateEmail(email) {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return emailRegex.test(email);
-    }
-
-
-    function validateForm(uName, uPswrd, uEmail) {
-        const regErrors = document.getElementById('regErrors');
-        regErrors.innerHTML = ''; // Clear previous errors
-        let errors = [];
-
-        if (!validateUsername(uName)) {
-            errors.push("Username must have all required parameters.");
-        }
-        if (!validatePassword(uPswrd)) {
-            errors.push("Password must have all required parameters.");
-        }
-        if (!validateEmail(uEmail)) {
-            errors.push("Invalid email.");
-        }
-
-        if (errors.length > 0) {
-            regErrors.innerHTML = errors.map(error => `<p>${error}</p>`).join('');
-            return false; // Return false if there are errors
-        }
-
-        return true; // Return true if all validations passed
-    }
-
-
     document.getElementById('registerBTN').addEventListener('click', async() => {
         const uName = await document.getElementById('uReg').value
         const uPswrd = await document.getElementById('pReg').value
