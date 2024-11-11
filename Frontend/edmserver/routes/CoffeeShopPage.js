04a3f5fec23181f666ff788cf396b6473838da70
UnLuckiestClovr <urluckyclovr@gmail.com>
2024-11-11 10:29:59 -0700

    Finished Cafe Page, testing needed.

--- Frontend/edmserver/routes/CoffeeShopPage.js
+++ Frontend/edmserver/routes/CoffeeShopPage.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+    const fList = getFood();
+    const dList = getDrinks();
+
     let boolLog = false
     if(req.session === undefined) {
         boolLog = false
@@ -13,9 +16,33 @@ router.get('/', function(req, res) {
             title: "The Cloverpatch",
             loggedInBool: boolLog,
             scriptName: "/javascripts/edm.js",
-            FoodList: [],
-            DrinkList: []
+            FoodList: fList,
+            DrinkList: dList
         })
 })
+
+async function getFood() {
+    const response = await fetch('http://localhost:12003/products/fetch/food', {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateInfo)
+    })
+
+    return JSON.parse(response).data
+}
+
+async function getDrinks() {
+    const response = await fetch('http://localhost:12003/products/fetch/drinks', {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateInfo)
+    })
+
+    return JSON.parse(response).data
+}
 
 module.exports = router;
